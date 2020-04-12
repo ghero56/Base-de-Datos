3cdfbece167252fb6743d7ef3f1889438892c164
Ghero56 <arcigaguzmanfernando@gmail.com>
2020-04-12 11:39:17 -0500

    Update BaseTrabajo.sql
    se crea la base de datos completa

--- Primer Version/BaseTrabajo.sql
+++ Primer Version/BaseTrabajo.sql
@@ -1,13 +1,38 @@
-create table PrimeraEtapa (
+CREATE TABLE PrimeraEtapa (
+    Folio int (10),
     Origen varchar (50),
-    Polisa int (50),
-    Lista int (50),
+    Polisa int (10),
+    Lista int (10),
     Deudor varchar (50),
     Direccion varchar (90),
-    Municipio varchar (90),
+    Municipio varchar (30),
     Adeudo int (7),
     Pago int (7),
     AdeudoPc int (7),
-    FSuscrip varchar (30),
-    FPago varchar(30)
+    FSuscrip varchar (20),
+    FPago varchar(20)
+);
+
+CREATE TABLE SegundaEtapa (
+    ExpedJudicial varchar (50),
+    Admite varchar (1),
+    FechaRequerimiento varchar (20),
+    Cedula varchar (1),
+    Oposicion varchar (1),
+    ComenOposicion varchar (90), --- en caso de ser falso
+    CitaFP varchar (20),
+    Oficio varchar (1),
+    Embargo int (4),
+    ComenEmbargo varchar (90), ---dependiendo lo que elija en el anterior
+    Emplazado varchar (1),
+    Dinero int (7) ---sera alguno de estos 3
+);
+
+CREATE TABLE TerceraEtapa (
+    Contesta varchar (1),
+    Rebeldia varchar (1),
+    Terceria varchar (1),
+    Audiencia varchar (20),
+    Sentencia varchar (90),
+    Incidente varchar (1)
 );
